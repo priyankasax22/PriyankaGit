f47ba6e759097776d0a222f60da751cbf2359b3c
manu shanker <priyankasax22@gmail.com>
2019-05-13 18:25:11 -0400

    my secont push

---
+++ src/test/java/mygitadda/git1.java
@@ -0,0 +1,14 @@
+package mygitadda;
+
+public class git1 {
+
+	public void tes1()
+	{
+		System.out.println("my first method");
+	}
+	
+	public void tes2()
+	{
+		System.out.println("my scond method");
+	}
+}
